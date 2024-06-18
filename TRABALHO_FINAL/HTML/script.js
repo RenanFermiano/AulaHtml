2c7a07dcb28c98c6db4aac9e16c93f8e4c6fbfa0
Renan <165823447+RenanFermiano@users.noreply.github.com>
2024-06-18 16:56:03 -0300

    aula 27

---
+++ TRABALHO_FINAL/HTML/script.js
@@ -0,0 +1,24 @@
+function addToCart(product) {
+    alert(product + " foi adicionado ao carrinho!");
+}
+
+// Carregar produtos dinamicamente
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('data/products.json')
+        .then(response => response.json())
+        .then(products => {
+            const productsContainer = document.querySelector('.products');
+            products.forEach(product => {
+                const productElement = document.createElement('div');
+                productElement.classList.add('product');
+                productElement.innerHTML = `
+                    <img src="${product.image}" alt="${product.name}">
+                    <h2>${product.name}</h2>
+                    <p>${product.description}</p>
+                    <p>Preço: €${product.price}</p>
+                    <button onclick="addToCart('${product.name}')">Adicionar ao Carrinho</button>
+                `;
+                productsContainer.appendChild(productElement);
+            });
+        });
+});
